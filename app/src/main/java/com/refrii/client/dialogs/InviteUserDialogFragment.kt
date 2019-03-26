71d31a2d9a8ce069d6097585fd02980830adf470
Yusuke Mukai <mukopikmin@gmail.com>
2019-03-27 01:24:24 +0900

    validte inviting email before submit

--- app/src/main/java/com/refrii/client/dialogs/InviteUserDialogFragment.kt
+++ app/src/main/java/com/refrii/client/dialogs/InviteUserDialogFragment.kt
@@ -9,8 +9,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import com.refrii.client.R
 import com.refrii.client.data.api.models.User
 
@@ -21,13 +25,13 @@ class InviteUserDialogFragment : DialogFragment() {
         val inflater = activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val content = inflater.inflate(R.layout.invite_user_dialog, null)
         val editText = content.findViewById<EditText>(R.id.editText)
+        val message = content.findViewById<TextView>(R.id.messageTextView)
         val bundle = arguments
         val sharedUsersEmail = bundle?.getStringArrayList("shared_users_email")
-
-        return AlertDialog.Builder(activity!!)
+        val dialog = AlertDialog.Builder(activity!!)
                 .setTitle("カテゴリの共有")
                 .setView(content)
-                .setPositiveButton(getString(R.string.message_share_box)) { _, _ ->
+                .setPositiveButton(getString(R.string.message_share_box)) { dialog, which ->
                     val intent = Intent()
                             .putExtra("email", editText.text.toString())
                     val pendingIntent = activity?.createPendingResult(targetRequestCode, intent, PendingIntent.FLAG_ONE_SHOT)
@@ -36,6 +40,29 @@ class InviteUserDialogFragment : DialogFragment() {
                 }
                 .setNegativeButton(getString(R.string.message_cancel)) { _, _ -> }
                 .create()
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = editText.text.toString()
+                val button = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+
+                if (sharedUsersEmail != null && sharedUsersEmail.contains(input)) {
+                    button.isEnabled = false
+                    message.visibility = View.VISIBLE
+                    message.text = "すでに共有されています"
+                } else {
+                    button.isEnabled = true
+                    message.visibility = View.GONE
+                    message.text = ""
+                }
+            }
+        }
+
+        message.visibility = View.GONE
+        editText.addTextChangedListener(textWatcher)
+
+        return dialog
     }
 
     companion object {
