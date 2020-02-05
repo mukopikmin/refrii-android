6977fed08992ec092f2f7ded09a31ff69b392647
Yusuke Mukai <mukopikmin@gmail.com>
2020-02-05 22:54:25 +0900

    Deploy staging app on every push

--- deploy.sh
+++ deploy.sh
@@ -8,37 +8,36 @@ mv temp/keystore.jks .
 
 # ./gradlew test
 
-if [[ "$TRAVIS_TAG" =~ ^[0-9\.]+$ ]] ; then
-  echo "Start production build."
-  ./gradlew generateLicensePage
-  ./gradlew assembleProduction;
-  jarsigner \
-    -verbose \
-    -storepass $KEYSTORE_ALIAS_PASSWORD \
-    -keypass $KEYSTORE_PASSWORD \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore keystore.jks \
-    -signedjar app/production/release/app-production-release.apk \
-    app/build/outputs/apk/production/release/app-production-release-unsigned.apk \
-    $KEYSTORE_ALIAS;
-  ./gradlew uploadDeployGateProductionRelease;
+if [ "$TRAVIS_BRANCH" = "master" ] ; then
+  if [[ "$TRAVIS_TAG" =~ ^[0-9\.]+$ ]] ; then
+    echo "Start production build."
+    ./gradlew generateLicensePage
+    ./gradlew assembleProduction;
+    jarsigner \
+      -verbose \
+      -storepass $KEYSTORE_ALIAS_PASSWORD \
+      -keypass $KEYSTORE_PASSWORD \
+      -sigalg SHA1withRSA \
+      -digestalg SHA1 \
+      -keystore keystore.jks \
+      -signedjar app/production/release/app-production-release.apk \
+      app/build/outputs/apk/production/release/app-production-release-unsigned.apk \
+      $KEYSTORE_ALIAS;
+    ./gradlew uploadDeployGateProductionRelease;
+  else
+    echo "Start staging build."
+    ./gradlew generateLicensePage
+    ./gradlew assembleDevelopment;
+    jarsigner \
+      -verbose \
+      -storepass $KEYSTORE_ALIAS_PASSWORD \
+      -keypass $KEYSTORE_PASSWORD \
+      -sigalg SHA1withRSA \
+      -digestalg SHA1 \
+      -keystore keystore.jks \
+      -signedjar app/development/release/app-development-release.apk \
+      app/build/outputs/apk/development/release/app-development-release-unsigned.apk \
+      $KEYSTORE_ALIAS;
+    ./gradlew uploadDeployGateDevelopmentRelease;
+  fi
 fi
-
-if [[ "$TRAVIS_TAG" =~ ^[0-9\.]+ ]] ; then
-  echo "Start staging build."
-  ./gradlew generateLicensePage
-  ./gradlew assembleDevelopment;
-  jarsigner \
-    -verbose \
-    -storepass $KEYSTORE_ALIAS_PASSWORD \
-    -keypass $KEYSTORE_PASSWORD \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore keystore.jks \
-    -signedjar app/development/release/app-development-release.apk \
-    app/build/outputs/apk/development/release/app-development-release-unsigned.apk \
-    $KEYSTORE_ALIAS;
-  ./gradlew uploadDeployGateDevelopmentRelease;
-fi
-
