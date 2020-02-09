59ea7020f89e5c3098131a2529cde6c378b0c17e
Yusuke Mukai <mukopikmin@gmail.com>
2020-02-09 23:44:04 +0900

    0.13.7

--- deploy.sh
+++ deploy.sh
@@ -8,23 +8,24 @@ mv temp/keystore.jks .
 
 # ./gradlew test
 
+if [[ $TRAVIS_TAG =~ ^[0-9\.]+$ ]] ; then
+  echo "Start production build."
+  ./gradlew generateLicensePage
+  ./gradlew assembleProduction;
+  jarsigner \
+    -verbose \
+    -storepass $KEYSTORE_ALIAS_PASSWORD \
+    -keypass $KEYSTORE_PASSWORD \
+    -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -keystore keystore.jks \
+    -signedjar app/production/release/app-production-release.apk \
+    app/build/outputs/apk/production/release/app-production-release-unsigned.apk \
+    $KEYSTORE_ALIAS;
+  ./gradlew uploadDeployGateProductionRelease;
+fi
+   
 if [ "$TRAVIS_BRANCH" = "master" ] ; then
-  if [[ "$TRAVIS_TAG" =~ ^[0-9\.]+$ ]] ; then
-    echo "Start production build."
-    ./gradlew generateLicensePage
-    ./gradlew assembleProduction;
-    jarsigner \
-      -verbose \
-      -storepass $KEYSTORE_ALIAS_PASSWORD \
-      -keypass $KEYSTORE_PASSWORD \
-      -sigalg SHA1withRSA \
-      -digestalg SHA1 \
-      -keystore keystore.jks \
-      -signedjar app/production/release/app-production-release.apk \
-      app/build/outputs/apk/production/release/app-production-release-unsigned.apk \
-      $KEYSTORE_ALIAS;
-    ./gradlew uploadDeployGateProductionRelease;
-  else
     echo "Start staging build."
     ./gradlew generateLicensePage
     ./gradlew assembleDevelopment;
@@ -38,6 +39,5 @@ if [ "$TRAVIS_BRANCH" = "master" ] ; then
       -signedjar app/development/release/app-development-release.apk \
       app/build/outputs/apk/development/release/app-development-release-unsigned.apk \
       $KEYSTORE_ALIAS;
-    ./gradlew uploadDeployGateDevelopmentRelease;
-  fi
+   ./gradlew uploadDeployGateDevelopmentRelease;
 fi
